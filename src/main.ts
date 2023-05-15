edf397414d4b23e378a811e0f77487911fa78a96
Joel Thomas <thomas.2@iitj.ac.in>
2023-05-16 04:19:13 +0530

    updater port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
@@ -10,6 +11,6 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
